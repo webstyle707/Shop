40a8df85c0affb9153ee6eee7942218c561f36cd
webstyle707 <webstyle707@gmail.com>
2024-08-30 15:48:31 +0800

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div><Header/></div>
         {children}
-        <div>hi</div>
+        <div><Footer/></div>
         </body>
     </html>
   );
